f5976d82516c70c3ecba301efb3e0225f571a4c1
arunmbalaji <42144058+arunmbalaji@users.noreply.github.com>
2020-03-11 10:48:06 +0530

    update
    create one more memory leak

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,10 +12,20 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        App ap = new App();
+        for(long ;i<1000;i++)
+        {
+            ap.addIncremental(i);
+        }
     }
 
     private final String getMessage() {
         return message;
     }
-
+    publiclong addIncremental(long l)
+    {
+        Long sum=0L;
+        sum =sum+l;
+        return sum;
+    }
 }
